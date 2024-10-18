1d6d0a8e320d8f3e73f732d48f46c8944ae8ad9f
M. Fahrianor <moraxz112@gmail.com>
2024-10-18 15:08:46 +0800

    p7

---
+++ Kelas/Pertemuan-7/pertemuan 7.py
@@ -0,0 +1,70 @@
+# #pertemuan 7
+
+# # def aku_fungsi():
+# #     print("hello world")
+
+# # def tambah (a,b):
+# #     hasil = a+b
+# #     return hasil
+# # tambah(2,4)
+
+# # tambahan = tambah(2,4) + tambah(4,6)
+# # print(tambahan)
+
+# # tambah(10,2)
+
+# def pilihan(opsi):
+#         match opsi:
+#               case 1:
+#                     print(1)
+#                     return
+#               case 2:
+#                     print(2)
+#                     return
+#         print(3)
+# # pilihan(2)
+
+# def cetak (nama,nim,*matkul):
+#        print(nama,nim,matkul)
+# cetak("fathir",47,"kalkulus","fisika")
+
+# var = 2,4,5
+# # print(type(var))
+# var1,var2 = var
+# print(var)
+# print(var1)
+# print(var2)
+
+# import os
+# os.system("cls")
+
+# # membuat variabel global
+# Nama = "Informatika"
+# Mata_Kuliah = "Algoritma dan Pemrograman Dasar"
+# kelas = "it asikk"
+# # membuat variabel lokal
+# def info():
+#     Nama = "Teknik Elektro"
+#     Mata_Kuliah = "Pengantar Teknik ELektro"
+    
+# # mengakses variabel lokal
+# print("Prodi:", Nama)
+# print("Mata Kuliah:", Mata_Kuliah)
+# # mengakses variabel global
+# print("Prodi:", Nama)
+# print("Mata Kuliah:", Mata_Kuliah)
+# print(kelas)
+
+# info()
+# # print("prodi:", Nama)
+# # print("mata kuliah:", Mata_Kuliah)
+
+
+# def cetak (nama,nim,umur = 18):
+#        print(nama,nim,umur)
+# cetak("fathir",47,112)
+
+
+
+
+
